6bba1b13a0029d46ca6ed4f9f832503be51186ca
Didulana Lokugamage <didulanalokugamage@gmail.com>
2026-02-21 22:34:31 +0530

    feat(frontend): add parent routes

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TrackingMap from "./pages/parent/LiveTracking";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LiveTracking from "./pages/parent/LiveTracking";
 import Notifications from "./pages/parent/Notifications";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TrackingMap />} />
-        <Route path="/tracking" element={<TrackingMap />} />
-        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/" element={<Navigate to="/parent/live-tracking" replace />} />
+        <Route path="/parent/live-tracking" element={<LiveTracking />} />
+        <Route path="/parent/notifications" element={<Notifications />} />
       </Routes>
     </BrowserRouter>
   );
