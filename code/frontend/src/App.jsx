e4434c795d3baa8209fd60df392c50f997366bb0
Samara <e22127@eng.pdn.ac.lk>
2026-03-13 22:35:35 +0530

    Task D2 admin dashboard UI

---
+++ code/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* default redirect */}
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+
+        {/* admin dashboard */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
